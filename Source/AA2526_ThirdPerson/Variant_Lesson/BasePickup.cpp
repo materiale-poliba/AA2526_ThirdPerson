6877fab86870330477f603747df4859088fe4e82
marcosecchi <752514+marcosecchi@users.noreply.github.com>
2026-03-30 19:50:17 +0200

    Add pickup.

---
+++ Source/AA2526_ThirdPerson/Variant_Lesson/BasePickup.cpp
@@ -0,0 +1,43 @@
+﻿#include "BasePickup.h"
+
+#include "Components/ArrowComponent.h"
+#include "Components/SphereComponent.h"
+
+ABasePickup::ABasePickup()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	ArrowComp = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	RootComponent = ArrowComp;
+	ArrowComp->ArrowColor = FColor::Yellow;
+	ArrowComp->ArrowLength = 200.f;
+	
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	SphereComp->SetupAttachment(RootComponent);
+	SphereComp->SetSphereRadius(500.f);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	SphereComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	
+	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ABasePickup::HandlePickupDelegate);
+	
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComp->SetupAttachment(SphereComp);
+}
+
+void ABasePickup::HandlePickupDelegate(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Pickup overlapped by %s"), *OtherActor->GetName());
+	// Tutti i check del caso
+	// Se sono passati, eseguo il pickup effettivo
+	HandlePickup();
+}
+
+void ABasePickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/AA2526_ThirdPerson/Variant_Lesson/BasePickup.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BasePickup.generated.h"
+
+class UArrowComponent;
+class USphereComponent;
+
+UCLASS()
+class AA2526_THIRDPERSON_API ABasePickup : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+//	TObjectPtr<UArrowComponent> ArrowComp;
+	UArrowComponent* ArrowComp;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	USphereComponent* SphereComp;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+public:
+	// Sets default values for this actor's properties
+	ABasePickup();
+
+	UFUNCTION()
+	void HandlePickupDelegate(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void HandlePickup();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
